4b1bf7e6a18cacdcb7fa17f7bb6b974bd23e9854
NirvanaFan23146 <Poro5hin.d@yandex.ru>
2025-04-28 00:57:15 +0500

    add blockcheck and some binaries

---
+++ blockcheck/blockcheck.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+cd /d "%~dp0"
+FOR /F "tokens=* USEBACKQ" %%F IN (`..\bin\cygwin\bin\cygpath -C OEM -a -m files\blog.sh`) DO (
+SET P='%%F'
+)
+
+"%~dp0..\bin\elevator.exe" ..\bin\cygwin\bin\bash.exe -i "%P%"
