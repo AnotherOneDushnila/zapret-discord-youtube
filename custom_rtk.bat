cb2842df493ec7fb68414c0201599e944e9a0d51
NirvanaFan23146 <Poro5hin.d@yandex.ru>
2025-07-21 23:56:15 +0500

    otmena kostyley!

--- custom_rtk.bat
+++ custom_rtk.bat
@@ -12,7 +12,7 @@ set "FAKE=%~dp0bin\fake\"
 set "LISTS=%~dp0lists\"
 
 start "zapret: %~n0" /min "%BIN%winws.exe" --wf-tcp=80,443,%GameFilter% --wf-udp=443,50000-50100,%GameFilter% ^
---filter-tcp=443 --hostlist-exclude="%LISTS%hostlist-exclude.txt" --dpi-desync=fakedsplit --dpi-desync-fooling=md5sig --dup=1 --dup-cutoff=n2 --dup-fooling=md5sig --dpi-desync-split-pos=1 --new ^
+--filter-tcp=443 --hostlist="%LISTS%hostlist-discord-youtube.txt" --dpi-desync=fakedsplit --dpi-desync-fooling=md5sig --dup=1 --dup-cutoff=n2 --dup-fooling=md5sig --dpi-desync-split-pos=1 --new ^
 --filter-udp=50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
 --filter-tcp=2099 --ipset="%LISTS%ipset-cloudflare.txt" --dpi-desync=syndata --new ^
 --filter-tcp=5222 --ipset="%LISTS%ipset-cloudflare.txt" --dpi-desync=syndata --new ^
