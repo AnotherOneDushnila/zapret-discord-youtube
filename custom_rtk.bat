c8f2c92111bb7e80a496e0b3593fcdb130365ca8
NirvanaFan23146 <Poro5hin.d@yandex.ru>
2025-12-01 20:33:22 +0500

    Here comes the airplane (googlemeet access)

--- custom_rtk.bat
+++ custom_rtk.bat
@@ -16,7 +16,7 @@ start "zapret: %~n0" /min "%BIN%winws.exe" --wf-tcp=80,443,%GameFilter% --wf-udp
 --filter-tcp=2053,2083,2087,2096,8443 --hostlist-domains=discord.media --dpi-desync=multisplit --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=1 --dpi-desync-split-seqovl-pattern="%FAKE%tls_clienthello_www_google_com.bin" --new ^
 --filter-tcp=443 --hostlist="%LISTS%list-general.txt" --dpi-desync=multisplit --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=1 --dpi-desync-split-seqovl-pattern="%FAKE%tls_clienthello_www_google_com.bin" --new ^
 --filter-udp=1400,19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
---filter-udp=1400 --filter-l7=stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
+--filter-udp=1400,3478,19302,19304,19305 --filter-l7=stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
 --filter-tcp=2099 --ipset="%LISTS%ipset-cloudflare.txt" --dpi-desync=syndata --new ^
 --filter-tcp=5222 --ipset="%LISTS%ipset-cloudflare.txt" --dpi-desync=syndata --new ^
 --filter-tcp=5223 --ipset="%LISTS%ipset-cloudflare.txt" --dpi-desync=syndata 
